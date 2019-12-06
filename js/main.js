c3aa1100d511f625984e2828a6914899cf0be5bf
Ralph Hovsepian <ralphhovsepian@mail.ru>
2019-12-06 17:13:39 +0400

    changed ui

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+
+
 const lastFifteen = document.getElementById('last-fifteen');
 const nextFive = document.getElementById('next-five');
 const nextFifteen = document.getElementById('next-fifteen');
 const gamesInfo = document.getElementById('gamesInfo');
 
-async function showGames(url) {
+
+
+async function showGames(choiceId, url) {
+    
+    activeLink();
 
     while (gamesInfo.firstChild) {
         gamesInfo.removeChild(gamesInfo.firstChild)
@@ -15,18 +21,50 @@ async function showGames(url) {
     data.events.forEach(event => {
         console.log(event);
 
-        let matchInfo = document.createElement('div');
-        matchInfo.id = 'matchInfo';
-        gamesInfo.appendChild(matchInfo);
-        let pElement = document.createElement('p');
-        pElement.id = event.idEvent;
-        pElement.innerHTML = `${event.strEvent} <br><br><br>${event.dateEvent}`;
-        matchInfo.appendChild(pElement);
+        let colsm = document.createElement('div');
+        colsm.className = 'col-sm-6';
+        gamesInfo.appendChild(colsm);
+
+        let cardDiv = document.createElement('div');
+        cardDiv.className = 'card bg-white text-dark';
+        colsm.appendChild(cardDiv);
+        
+        let cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+        cardDiv.appendChild(cardBody);
+
+        let heading = document.createElement('h5');
+        heading.className = 'card-title';
+        heading.innerHTML = event.strEvent;
+
+        let paragraph = document.createElement('p');
+        paragraph.className = 'card-text';
+        paragraph.innerHTML = event.dateEvent;
+
+        let moreInfo = document.createElement('a');
+        moreInfo.className = 'btn btn-primary';
+        moreInfo.href = '#';
+        moreInfo.innerText  = 'More info';
+        cardBody.append(heading, paragraph, moreInfo);
     });
     
 }
 
+//changes active link class when clicked
+ const activeLink = () => {
 
-lastFifteen.onclick = () => console.log(showGames('https://www.thesportsdb.com/api/v1/json/1/eventspastleague.php?id=4328'));
-nextFive.onclick = () => console.log(showGames('https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id=133602'));
-nextFifteen.onclick = () => console.log(showGames('https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4328'));
+    const activeLink = document.getElementsByClassName('nav-link');
+    for (let i = 0; i < activeLink.length; i++) {
+        activeLink[i].addEventListener("click", function() {
+          let current = document.getElementsByClassName("active");
+          current[0].className = 'nav-link';
+          this.className += " active";
+        });
+      }
+
+ }
+
+
+lastFifteen.onclick = () => console.log(showGames(lastFifteen.id, 'https://www.thesportsdb.com/api/v1/json/1/eventspastleague.php?id=4328'));
+nextFive.onclick = () => console.log(showGames(nextFive.id, 'https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id=133602'));
+nextFifteen.onclick = () => console.log(showGames(nextFifteen.id, 'https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4328'));
